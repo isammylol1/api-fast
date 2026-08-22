ea7269df7d2519a2311d1e468e633edcaa293609
isammylol1 <samuelcorreacontato@gmail.com>
2026-08-21 21:06:36 -0300

    Delete API_FAST_AI directory

--- API_FAST_AI/models/cliente.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Cliente(BaseModel):
-    nome: str
-    idade: int
-    tel: str
-    cpf: str
--- API_FAST_AI/models/restaurante.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Restaurante(BaseModel):
-    nome: str
-    localizaçao: str
-    
